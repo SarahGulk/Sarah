85fe27ea007bbe57d4372ba9655baf2956400c32
Avital Greenberg <34717045+teegreen18@users.noreply.github.com>
2018-05-17 11:37:50 -0400

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,6 +2,19 @@ public abstract class Item implements Library {
     String title;
     int ISBN;
     int libraryRecordNumber;
+    boolean checkedOut;
+    boolean onHold;
+    int dueDate;
+    
+    public abstract int createDueDate();
+    
+    public void checkOut() {this.checkedOut = true;}
+    
+    public void hold() {this.onHold = true;}
+    
+    public void returnItem() {this.checkedOut = false;}
+    
+    public void takeOffHold() {this.onHold = false;}
 
     public void setISBN(int ISBN) {
         this.ISBN = ISBN;
