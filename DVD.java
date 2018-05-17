dd01c825761c68b3c52543037067fe83cf8d3f4c
Avital Greenberg <34717045+teegreen18@users.noreply.github.com>
2018-05-17 11:31:41 -0400

    Update DVD.java

--- DVD.java
+++ DVD.java
@@ -1,6 +1,16 @@
 public class DVD extends Item {
 
-    public DVD(String title) {
+    public DVD(String title, int isbn) {
         this.title = title;
+        this.isbn = isbn;
+        this.checkedOut = false;
+        this.onHold = false;
+        this.dueDate = createDueDate();
+    }
+    
+    @Override
+    public int createDueDate() {
+        //DVD's can be taken out for 5 days
+        return 5;
     }
 }
