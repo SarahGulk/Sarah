bf8944c44272464cb4f016729ab5c0980a27e5e3
Avital Greenberg <34717045+teegreen18@users.noreply.github.com>
2018-05-17 11:35:13 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -2,9 +2,19 @@ public class Book extends Item {
     int copyAmount;
     String author, title;
 
-    public Book(String author, String title) {
+    public Book(String author, String title, int isbn) {
         this.author = author;
         this.title = title;
+        this.isbn = isbn;
+        this.checkedOut = false;
+        this.onHold = false;
+        this.dueDate = createDueDate();
+    }
+    
+    @Override 
+    public int createDueDate() {
+        //books can be taken out for two weeks at a time
+        return 14;
     }
 
     public String getAuthor() {
